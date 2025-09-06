aa52da6babb5e1e191d9987c1b597288fb3d2784
Andre Terron <git@terron.me>
2025-09-06 06:02:49 -0700

    Silence react router error with chrome devtools

--- apps/website/app/routes.ts
+++ apps/website/app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
+import { Route } from 'react-router';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index('routes/home.tsx'),
+  route(
+    '.well-known/appspecific/com.chrome.devtools.json',
+    'routes/not-found.tsx'
+  ),
+] satisfies RouteConfig;
